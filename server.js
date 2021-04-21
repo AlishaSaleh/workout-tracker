6a540826bb62c56b1a49a3a56263169bed58dc73
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-04-21 16:19:52 +0100

    Working but not adding to database

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const logger = require("morgan");
+const routes = require('./routes');
 
+// const path = require("path");
 // require path? fs?
 
 const PORT = process.env.PORT || 3000;
@@ -21,6 +23,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // routes for STATS, EXERCISE
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
