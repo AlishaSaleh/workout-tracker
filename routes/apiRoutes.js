4f528ce97fb39f376d6045828190b98dcb174da7
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-04-21 11:42:52 +0100

    API routes added

---
+++ routes/apiRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const Workout = require('../models/Workout');
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({ params, body }, res) => {
+    Workout.findOneAndUpdate( //params.id
+        { _id: params.id },
+        { $push: { exercise: body } },
+        { new: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
